94e4231a6d5a3ae28816d0706b42f84043586007
fernandevdaza <fernando.daza.18@outlook.com>
2025-03-17 10:58:18 -0400

    Fixed DB Script

--- app/scripts_sql/create_tables.sql
+++ app/scripts_sql/create_tables.sql
@@ -1,67 +1,69 @@
 CREATE TABLE `Usuarios` (
-  `id_usuario` bigint(20) auto_increment,
-  `nombre_usuario` varchar(20),
-  `apellido_usuario` varchar(20),
-  `fecha_nacimiento_usuario` date,
-  `email_usuario` varchar(30),
-  `contrasenia_usuario` varchar(15),
-  `fecha_registro_usuario` date,
-  `codigo_estudiante_upb` bigint(5),
-  PRIMARY KEY (`id_usuario`)
+  `id_usuario` bigint(20) auto_increment PRIMARY KEY,
+  `nombre_usuario` varchar(20) NOT NULL,
+  `apellido_usuario` varchar(20) NOT NULL,
+  `fecha_nacimiento_usuario` date NOT NULL,
+  `email_usuario` varchar(100) NOT NULL UNIQUE,
+  `contrasenia_usuario` varchar(255) NOT NULL,
+  `fecha_registro_usuario` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `codigo_estudiante_upb` bigint(5) UNIQUE
 );
 
 CREATE TABLE `Libreros` (
-  `id_librero` bigint(20) auto_increment,
-  `Usuarios_id_usuario` bigint(20),
-  PRIMARY KEY (`id_librero`),
-    FOREIGN KEY (`Usuarios_id_usuario`) references Usuarios(id_usuario)
+  `id_librero` bigint(20) auto_increment PRIMARY KEY,
+  `Usuarios_id_usuario` bigint(20) UNIQUE NOT NULL,
+  FOREIGN KEY (`Usuarios_id_usuario`) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE
 );
 
-
-
 CREATE TABLE `Libros` (
-  `id_libro` bigint(20) auto_increment,
-  `isbn` varchar(20),
-  `titulo_libro` varchar(30),
-  `descripcion_libro` text,
-  `imagen_libro` text,
-  `fecha_publicacion_libro` date,
-  `enlace_libro` text,
-  PRIMARY KEY (`id_libro`)
+  `id_libro` bigint(20) auto_increment PRIMARY KEY,
+  `titulo_libro` varchar(100) NOT NULL,
+  `descripcion_libro` text NOT NULL,
+  `imagen_libro` text NOT NULL,
+  `fecha_publicacion_libro` date NOT NULL
+);
+
+CREATE TABLE `Ediciones` (
+  `id_edicion` bigint(20) auto_increment PRIMARY KEY,
+  `libros_id_libro` bigint(20) NOT NULL,
+  `isbn` varchar(13) UNIQUE NOT NULL,
+  `enlace_libro` text NOT NULL,
+  `fecha_edicion` date NOT NULL,
+  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`) ON DELETE CASCADE
 );
 
 CREATE TABLE `Autores` (
-  `id_autor` bigint(20) auto_increment,
-  `nombre_autor` varchar(20),
-  `apellido_autor` varchar(20),
-  `fecha_nacimiento_autor` date,
-  PRIMARY KEY (`id_autor`)
+  `id_autor` bigint(20) auto_increment PRIMARY KEY,
+  `nombre_autor` varchar(50) NOT NULL,
+  `apellido_autor` varchar(50) NOT NULL,
+  `fecha_nacimiento_autor` date NOT NULL
 );
 
 CREATE TABLE `Libreros_Libros` (
-  `libreros_id_librero` bigint(20) ,
+  `libreros_id_librero` bigint(20),
   `libros_id_libro` bigint(20),
-  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`),
-  FOREIGN KEY (`libreros_id_librero`) REFERENCES `Libreros`(`id_librero`)
+  PRIMARY KEY (`libreros_id_librero`, `libros_id_libro`),
+  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`) ON DELETE CASCADE,
+  FOREIGN KEY (`libreros_id_librero`) REFERENCES `Libreros`(`id_librero`) ON DELETE CASCADE
 );
 
 CREATE TABLE `Libros_Autores` (
   `libros_id_libro` bigint(20),
   `autores_id_autor` bigint(20),
-  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`),
-  FOREIGN KEY (`autores_id_autor`) REFERENCES `Autores`(`id_autor`)
+  PRIMARY KEY (`libros_id_libro`, `autores_id_autor`),
+  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`) ON DELETE CASCADE,
+  FOREIGN KEY (`autores_id_autor`) REFERENCES `Autores`(`id_autor`) ON DELETE CASCADE
 );
 
 CREATE TABLE `Categorias` (
-  `id_categoria` bigint(20) auto_increment,
-  `nombre_categoria` varchar(20),
-  PRIMARY KEY (`id_categoria`)
+  `id_categoria` bigint(20) auto_increment PRIMARY KEY,
+  `nombre_categoria` varchar(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE `Libros_Categorias` (
   `libros_id_libro` bigint(20),
   `categorias_id_categoria` bigint(20),
-  FOREIGN KEY (`categorias_id_categoria`) REFERENCES `Categorias`(`id_categoria`),
-  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`)
+  PRIMARY KEY (`libros_id_libro`, `categorias_id_categoria`),
+  FOREIGN KEY (`categorias_id_categoria`) REFERENCES `Categorias`(`id_categoria`) ON DELETE CASCADE,
+  FOREIGN KEY (`libros_id_libro`) REFERENCES `Libros`(`id_libro`) ON DELETE CASCADE
 );
-
